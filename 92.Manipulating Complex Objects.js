fa9e2d6ec3cfa6aef9a2a701b6b44e6885367e5b
Stacy <stacy.from.mars@gmail.com>
2023-05-25 23:18:29 +0300

    lesson 92

---
+++ 92.Manipulating Complex Objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "30 Seconds to Mars",
+      "title": "30 Seconds to Mars",
+      "release_year": 2002,
+      "formats": [
+        "CD",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
